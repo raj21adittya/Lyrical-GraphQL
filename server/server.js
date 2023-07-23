0d36e9bfea7dc59622a12287c3e0097730e4675d
Adittya Raj <raj21adittya@gmail.com>
2023-07-23 19:11:21 +0530

    added mongo

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your Mongo Atlas URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://raj21adittya:OTPmexNShjk3uSt5@cluster0.ealtsba.mongodb.net/lyrical?retryWrites=true&w=majority';
 if (!MONGO_URI) {
   throw new Error('You must provide a Mongo Atlas URI');
 }
